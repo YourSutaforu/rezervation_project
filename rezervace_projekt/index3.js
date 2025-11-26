8f81e63d6fb2d8b8da31d510f2ff13323c9f9d9f
Sutaforu <vivinlolzakasora@gmail.com>
2025-11-26 21:04:43 +0100

    Add files via upload

---
+++ rezervace_projekt/index3.js
@@ -0,0 +1,71 @@
+function calendar_switch(elem){
+    let swich_count = 0
+    element = document.querySelector(elem)
+    if (swich_count == 0){
+        element.innerHTML += `
+        <button class = 'prev_month'></button>
+        <button class = 'next_month'></button>
+        `
+    }
+}
+
+function createCalendar(elem, year, month) {
+    let days = ["po", "út", "st", "čt", "pá", "so", "ne"];
+
+    // какой сегодня день месяца
+    let today = new Date().getDate();
+    let todayMonth = new Date().getMonth();
+    let todayYear = new Date().getFullYear();
+
+    // с какого дня недели начинается месяц (1–7)
+    let firstDay = new Date(year, month, 1).getDay();
+    if (firstDay === 0) firstDay = 7;
+
+    // сколько дней в месяце
+    let daysInMonth = new Date(year, month, 0).getDate();
+
+    let calendar = `<table><tr class="calendar_base">`;
+
+    // заголовки
+    for (let d of days) {
+        calendar += `<th>${d}</th>`;
+    }
+    calendar += `</tr>`;
+
+    let day = 1;
+
+    for (let i = 0; i < 6; i++) {
+        calendar += `<tr>`;
+
+        for (let j = 1; j <= 7; j++) {
+            if (i === 0 && j < firstDay) {
+                // пустые ячейки до первого дня месяца
+                calendar += "<td class = 'empty'></td>";
+            } else if (day > daysInMonth) {
+                // после окончания месяца
+                calendar += "<td class = 'empty'></td>";
+            } else {
+                // обычное число
+                let cls = "";
+
+                // подсветить сегодняшний день
+                if (day === today && month === todayMonth && year === todayYear) {
+                    cls = ` class="today"`;
+                }
+
+                calendar += `<td${cls}>${day}</td>`;
+                day++;
+            }
+        }
+
+        calendar += `</tr>`;
+    }
+
+    calendar += `</table>`;
+
+    document.querySelector(elem).innerHTML += calendar;
+}
+current_date = new Date()
+
+// пример:
+createCalendar("body", current_date.getFullYear(), current_date.getMonth());
