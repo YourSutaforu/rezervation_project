7b2b3608958dc0da0e858acb0450093fb79d4d3d
Sutaforu <vivinlolzakasora@gmail.com>
2025-12-29 22:41:01 +0100

    Delete rezervace_projekt/index.php

--- rezervace_projekt/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$time_list = [$hour_start = $_GET("hour_start"), $minute_start = $_GET("minute_start"), $hour_end = $_GET("hour_end"), $minute_end = $_GET("minute_end")];
-
-foreach ($time_unit as $time_list) {
-    if (strlen($time_unit) == 1):
-        $time_unit = "0" . $time_unit;
-}
