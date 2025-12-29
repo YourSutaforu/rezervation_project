58417d847c6023369f19acc18c29218342f64483
Sutaforu <vivinlolzakasora@gmail.com>
2025-12-29 22:44:36 +0100

    Add files via upload

---
+++ rezervace_projekt/database/reservations.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Дек 29 2025 г., 22:43
+-- Версия сервера: 10.4.32-MariaDB
+-- Версия PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `reservations`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `room_res`
+--
+
+CREATE TABLE `room_res` (
+  `room_name` mediumtext NOT NULL,
+  `person_name` mediumtext NOT NULL,
+  `person_id` int(7) NOT NULL,
+  `res_time_start` tinytext NOT NULL,
+  `res_time_end` tinytext NOT NULL,
+  `res_date` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user_list`
+--
+
+CREATE TABLE `user_list` (
+  `person_name` mediumtext NOT NULL,
+  `person_password` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
