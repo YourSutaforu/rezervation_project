ac0555cbfab61ae3f9df4deac2beaa1284ef8c99
Sutaforu <vivinlolzakasora@gmail.com>
2025-12-30 11:42:22 +0100

    Update big_room_calendar.php

--- rezervace_projekt_example/big_room_calendar.php
+++ rezervace_projekt_example/big_room_calendar.php
@@ -196,7 +196,7 @@ if (isset($_POST['big_room_res_Submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>rezervace mistnosti 80^2</title>
     <link rel="stylesheet" href="index1.css">
 </head>
 <body>
