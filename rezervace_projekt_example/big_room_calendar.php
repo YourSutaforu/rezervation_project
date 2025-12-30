dee46622bf5bb53e4dacc680c9d7556414e60ebf
Sutaforu <vivinlolzakasora@gmail.com>
2025-12-30 10:36:24 +0100

    Update big_room_calendar.php

--- rezervace_projekt_example/big_room_calendar.php
+++ rezervace_projekt_example/big_room_calendar.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "reservations_example");
+$conn = mysqli_connect("localhost", "root", "", "reservations");
 
 $reservation_date = '';
 $person_name = '';
